904515a29b2e4c7ff5ac8c66372cbacd89780f66
BalkissaT <55194844+BalkissaT@users.noreply.github.com>
2024-02-14 18:22:28 -0500

    Update srcipt.js

--- srcipt.js
+++ srcipt.js
@@ -1,20 +1,24 @@
-function showElement(elementId, delay) {
-    setTimeout(() => {
-        const element = document.getElementById(elementId);
-        element.classList.remove('hidden');
-        element.style.opacity = 1; // Pour l'animation fadeIn
-    }, delay);
+function showElement(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden'); // Cela déclenchera l'animation CSS fadeIn
+}
+
+function hideElement(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Définition des délais pour chaque étape de l'animation
-    showElement('hi', 0); // Immédiatement
-    showElement('just-wanted', 2000); // Après 2 secondes
-    showElement('funny-love', 4000); // Après 4 secondes
-    showElement('happy', 6000); // Après 6 secondes
-    showElement('valentine', 8000); // Après 8 secondes
-    showElement('kind', 10000); // Après 10 secondes
-    showElement('love', 12000); // Après 12 secondes
-    showElement('bunny-love', 14000); // Après 14 secondes
-    showElement('sign-off', 16000); // Après 16 secondes
+    const elementsToShow = ['hi', 'just-wanted', 'funny-love', 'happy', 'valentine', 'kind', 'love', 'bunny-love', 'sign-off'];
+    let delay = 0;
+
+    elementsToShow.forEach((id) => {
+        setTimeout(() => showElement(id), delay);
+        // Augmenter le délai pour le prochain élément
+        delay += 2000;
+    });
+
+     elementsToShow.forEach((id) => {
+        setTimeout(() => hideElement(id), delay + 2000); // Les éléments disparaîtront après 2 secondes supplémentaires
+     });
 });
